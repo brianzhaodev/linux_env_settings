349177dfefe1a086342a5cc4a9ecafe494037ebc
GoodNightGG <isu10203112a@gmail.com>
2024-07-01 15:17:41 +0800

    fix: fix the powerlevel10k dict permission issue

--- .env_setup.sh
+++ .env_setup.sh
@@ -4,7 +4,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 chsh -s $(which zsh)
 
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
