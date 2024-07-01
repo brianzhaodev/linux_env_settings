325f008c3934d0044766b4d5e6f632f5d7820f39
GoodNightGG <isu10203112a@gmail.com>
2024-07-01 15:08:41 +0800

    feat: project init

---
+++ .env_setup.sh
@@ -0,0 +1,16 @@
+[ ! -f /bin/zsh ] && sudo apt install -y zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+chsh -s $(which zsh)
+
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+curl https://raw.githubusercontent.com/brianzhaodev/linux_env_settings/main/.p10k.zsh > .p10k.zsh
+curl https://raw.githubusercontent.com/brianzhaodev/linux_env_settings/main/.zshrc.zsh > .zshrc.zsh
+
+exec zsh -l
---
+++ README.md
@@ -0,0 +1,12 @@
+# Linux Setup Script For Zsh Environment
+
+This repository contains a script to set up your Linux environment with essential configurations and tools specifically for Zsh.
+
+## How to Use
+
+To execute the setup script, use the following command in your terminal. This command will download the script and execute it directly.
+
+### Command to Run
+
+```sh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/brianzhaodev/linux_env_settings/main/.env_setup.sh)"
